a849ca0e3f0616d6438f8fe2b774cfcd342d97d0
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-04-20 18:06:59 +0900

    CodingTest

--- Programmers/lv1/lv1_핸드폰 번호 가리기.java
+++ Programmers/lv1/lv1_핸드폰 번호 가리기.java
@@ -1,18 +1,14 @@
 https://programmers.co.kr/learn/courses/30/lessons/12948
 
 class Solution {
-	public String solution(String phone_number) {
-		String answer = "";
-
-		for (int i = 0; i < phone_number.length() - 4; i++) {
-			answer += "*";
-		}
-		answer += phone_number.substring(phone_number.length() - 4);
-
-		return answer;
-	}
-<<<<<<< HEAD
+    public String solution(String phone_number) {
+        String answer = "";
+        
+        for(int i = 0; i < phone_number.length() - 4; i ++){
+          answer += "*";  
+        }
+        answer += phone_number.substring(phone_number.length()-4);
+        
+        return answer;
+    }
 }
-=======
-}
->>>>>>> a9d136510c496960db5515689f23f6429eff1525
