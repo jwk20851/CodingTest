66ae2fa4b4e77d6cfd8f558097fa8f5827925c5f
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-05-06 22:57:18 +0900

    Create lv1_자연수 뒤집어 배열로 만들기.java

---
+++ Programmers/lv1/lv1_자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,20 @@
+https://programmers.co.kr/learn/courses/30/lessons/12932
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        StringBuilder sb = new StringBuilder(s);
+        sb = sb.reverse();
+        
+        String[] ss = sb.toString().split("");
+        int[] answer = new int[ss.length];
+        
+        for (int i=0; i<ss.length; i++) {
+            answer[i] = Integer.parseInt(ss[i]);
+        }
+        
+        return answer;
+    }
+}
