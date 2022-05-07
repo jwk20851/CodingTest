611426e0d693453b09cb5f8abda48a3391368045
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-05-08 00:34:08 +0900

    Create lv1_문자열을 정수로 바꾸기.java

---
+++ Programmers/lv1/lv1_문자열을 정수로 바꾸기.java
@@ -0,0 +1,9 @@
+https://programmers.co.kr/learn/courses/30/lessons/12925
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
