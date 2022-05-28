67a1edd9273e0d3a649bab452ccfcf1ee170339f
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-05-28 20:57:31 +0900

    Create x만큼 간격이 있는 n개의 숫자.cs

---
+++ Programmers/C#/lv1/x만큼 간격이 있는 n개의 숫자.cs
@@ -0,0 +1,17 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12954?language=csharp
+
+public class Solution
+{
+    public long[] solution(int x, int n)
+    {
+        long[] answer = new long[n];
+
+        answer[0] = x;
+        for (int i = 1; i < n; i++)
+        {
+            answer[i] = answer[i - 1] + x;
+        }
+
+        return answer;
+    }
+}
