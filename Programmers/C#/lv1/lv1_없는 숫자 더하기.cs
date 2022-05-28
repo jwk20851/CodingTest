d41aa981d3a65937dde3176e340fc3fd619b08e4
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-05-29 00:31:55 +0900

    Create lv1_없는 숫자 더하기.cs

---
+++ Programmers/C#/lv1/lv1_없는 숫자 더하기.cs
@@ -0,0 +1,20 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/86051
+
+using System;
+
+public class Solution
+{
+    public int solution(int[] numbers)
+    {
+        int answer = -1;
+        int sum = 45;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum -= numbers[i];
+        }
+        answer = sum;
+
+        return answer;
+    }
+}
