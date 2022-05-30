aeb8f6c57fe20f476e6e9246277e95a91f14f731
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-05-30 22:26:41 +0900

    Create lv1_2016년.cs

---
+++ Programmers/C#/lv1/lv1_2016년.cs
@@ -0,0 +1,19 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12901?language=csharp
+
+using System;
+
+public class Solution
+{
+    public string solution(int a, int b)
+    {
+        string answer = "";
+
+        DateTime date = new DateTime(2016, a, b);
+        //Console.WriteLine(date);  // 5/24/2016 12:00:00 AM
+        answer = date.DayOfWeek.ToString();
+        //Console.WriteLine(answer);    // 	Tuesday
+        answer = answer.Substring(0, 3).ToUpper();
+
+        return answer;
+    }
+}
