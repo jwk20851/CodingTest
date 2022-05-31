f49ca769f148d9b86c25799cbe61e608a4973630
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-05-31 21:13:01 +0900

    Create lv1_두 개 뽑아서 더하기.cs

---
+++ Programmers/C#/lv1/lv1_두 개 뽑아서 더하기.cs
@@ -0,0 +1,26 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/68644
+
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int[] numbers)
+    {
+        List<int> answer = new List<int>();
+
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                sum = numbers[i] + numbers[j];
+                if (!(answer.Contains(sum)))
+                    answer.Add(sum);
+            }
+        }
+        answer.Sort();
+
+        return answer.ToArray();
+    }
+}
