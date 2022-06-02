79c7f1f2e4a4d92852676ad1735f24c1926e4a66
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-02 12:23:13 +0900

    Create lv1_예산.cs

---
+++ Programmers/C#/lv1/lv1_예산.cs
@@ -0,0 +1,20 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12982?language=csharp
+
+using System;
+
+public class Solution
+{
+    public int solution(int[] d, int budget)
+    {
+        int answer = 0;
+
+        Array.Sort(d);
+        foreach (int item in d)
+        {
+            if ((budget -= item) >= 0)
+                answer++;
+        }
+
+        return answer;
+    }
+}
