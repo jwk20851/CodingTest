61c5933231a734c0d162ac92083e1b4dbb7b6113
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-10 00:58:42 +0900

    Create lv1_핸드폰 번호 가리기.cs

---
+++ Programmers/C#/lv1/lv1_핸드폰 번호 가리기.cs
@@ -0,0 +1,23 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12948?language=csharp
+
+public class Solution
+{
+    public string solution(string phone_number)
+    {
+        string answer = "";
+
+        for (int i = 0; i < phone_number.Length; i++)
+        {
+            if (i < phone_number.Length - 4)
+            {
+                answer += "*";
+            }
+            else
+            {
+                answer += phone_number[i];
+            }
+        }
+        // 높은 버전에서 string[^4..]: ^ 역순, 4.. 4까지 자르기
+        return answer;
+    }
+}
