407d3712aa7b24587146cc88b19351cb10258ff2
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-15 21:18:21 +0900

    Create lv1_하샤드 수.cs

---
+++ Programmers/C#/lv1/lv1_하샤드 수.cs
@@ -0,0 +1,21 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12947?language=csharp
+
+public class Solution
+{
+    public bool solution(int x)
+    {
+        bool answer = true;
+
+        string temp = x.ToString();
+        int sum = 0;
+
+        foreach (char c in temp)
+        {
+            sum += int.Parse(c.ToString());
+        }
+
+        answer = (x % sum == 0) ? true : false;
+
+        return answer;
+    }
+}
