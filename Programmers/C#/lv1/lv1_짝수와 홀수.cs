47ed53ca6130b6d76563b1ee53e0372e62cfc298
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-16 20:57:34 +0900

    Create lv1_짝수와 홀수.cs

---
+++ Programmers/C#/lv1/lv1_짝수와 홀수.cs
@@ -0,0 +1,11 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12937?language=csharp
+
+public class Solution
+{
+    public string solution(int num)
+    {
+        string answer = "";
+        answer = (num % 2 == 0) ? "Even" : "Odd";
+        return answer;
+    }
+}
