ae894d61ef02d77487d271f1114590cb9ad2a67d
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-16 20:57:45 +0900

    Create lv1_평균 구하기.cs

---
+++ Programmers/C#/lv1/lv1_평균 구하기.cs
@@ -0,0 +1,18 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12944?language=csharp
+
+public class Solution
+{
+    public double solution(int[] arr)
+    {
+        double answer = 0;
+
+        int sum = 0;
+        foreach (int n in arr)
+        {
+            sum += n;
+        }
+        answer = (double)sum / arr.Length;
+
+        return answer;
+    }
+}
