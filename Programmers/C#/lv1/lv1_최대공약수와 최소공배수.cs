3eadf24d06f2d351e635b3e456f60eaba9a4c1cd
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-16 20:57:52 +0900

    Create lv1_최대공약수와 최소공배수.cs

---
+++ Programmers/C#/lv1/lv1_최대공약수와 최소공배수.cs
@@ -0,0 +1,25 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12940?language=csharp
+
+public class Solution
+{
+    public int[] solution(int n, int m)
+    {
+        int[] answer = new int[2];
+        int gcd = 0;
+
+        if (m != 0)
+            gcd = GCD(n, m);
+        else
+            gcd = n;
+
+        answer[0] = gcd;
+        answer[1] = n * m / gcd;
+
+        return answer;
+    }
+
+    public int GCD(int n, int m)
+    {
+        return (n % m == 0) ? m : GCD(m, n % m);
+    }
+}
