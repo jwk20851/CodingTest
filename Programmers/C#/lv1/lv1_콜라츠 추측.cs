07eb576d27986da9e4a3c7cac104ea6aa8372bf5
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-16 20:57:58 +0900

    Create lv1_콜라츠 추측.cs

---
+++ Programmers/C#/lv1/lv1_콜라츠 추측.cs
@@ -0,0 +1,33 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12943?language=csharp
+
+public class Solution
+{
+    public int solution(int num)
+    {
+        int answer = -1;
+        
+        long n = num;
+        int ctr = 0;
+        if (n > 1)
+        {
+            while (ctr < 500)
+            {
+                if (n % 2 == 0)
+                    n = n / 2;
+                else
+                    n = n * 3 + 1;
+
+                ctr++;
+                if (n == 1)
+                {
+                    answer = ctr;
+                    break;
+                }
+            }
+        }
+        else
+            return 0;
+
+        return answer;
+    }
+}
