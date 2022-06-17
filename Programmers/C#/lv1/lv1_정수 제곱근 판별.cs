220e6bea9efd0e4d80cad97e3cc22d8894ac50e0
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-17 17:37:09 +0900

    Create lv1_정수 제곱근 판별.cs

---
+++ Programmers/C#/lv1/lv1_정수 제곱근 판별.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Solution
+{
+    public long solution(long n)
+    {
+        long answer = 0;
+
+        long tmp = (long)Math.Sqrt(n);
+        answer = (tmp * tmp == n) ? (tmp + 1) * (tmp + 1) : -1;
+
+        return answer;
+    }
+}
