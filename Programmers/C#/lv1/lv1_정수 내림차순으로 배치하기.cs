44aa591d00137bbe86cb2987d22ea93e21c42431
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-17 17:45:54 +0900

    Create lv1_정수 내림차순으로 배치하기.cs

---
+++ Programmers/C#/lv1/lv1_정수 내림차순으로 배치하기.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution
+{
+    public long solution(long n)
+    {
+        long answer = 0;
+
+        String str = n.ToString();
+        Char[] temp = str.ToCharArray();
+
+        Array.Sort(temp);
+        Array.Reverse(temp);
+
+        answer = long.Parse(temp);
+
+        return answer;
+    }
+}
