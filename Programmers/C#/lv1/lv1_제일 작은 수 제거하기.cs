03ab0b7e0be384508a28b9573b647d59b6bafa58
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-17 17:27:29 +0900

    Create lv1_제일 작은 수 제거하기.cs

---
+++ Programmers/C#/lv1/lv1_제일 작은 수 제거하기.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int[] arr)
+    {
+        int[] answer = new int[] { };
+
+        if (arr.Length == 1)
+        {
+            answer = new int[] { -1 };
+            return answer;
+        }
+
+        List<int> list = new List<int>();
+        int min = arr[0];
+        foreach (int i in arr)
+        {
+            list.Add(i);
+            if (min >= i)
+                min = i;
+        }
+        list.Remove(min);
+        answer = list.ToArray();
+
+        return answer;
+    }
+}
