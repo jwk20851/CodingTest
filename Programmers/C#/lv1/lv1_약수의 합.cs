1627452f656e4c90641f0d22cfe216004e5a07e3
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-19 17:20:08 +0900

    Create lv1_약수의 합.cs

---
+++ Programmers/C#/lv1/lv1_약수의 합.cs
@@ -0,0 +1,22 @@
+﻿https://programmers.co.kr/learn/courses/30/lessons/12928?language=csharp
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+
+        if (n == 0) return 0;
+
+        int sum = 0, ctr = 1;
+        while (ctr != n)
+        {
+            if (n % ctr == 0)
+                sum += ctr;
+            ctr++;
+        }
+        answer = sum + n;
+
+        return answer;
+    }
+}
