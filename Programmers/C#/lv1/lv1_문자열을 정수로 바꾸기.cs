19231d2cd4024f34bf5b442243f3fae66b9af14e
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-20 17:29:12 +0900

    Create lv1_문자열을 정수로 바꾸기.cs

---
+++ Programmers/C#/lv1/lv1_문자열을 정수로 바꾸기.cs
@@ -0,0 +1,10 @@
+https://programmers.co.kr/learn/courses/30/lessons/12925?language=csharp
+
+public class Solution
+{
+    public int solution(string s)
+    {
+        int answer = int.Parse(s);
+        return answer;
+    }
+}
