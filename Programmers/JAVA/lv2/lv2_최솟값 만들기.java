c6cde440d011286b2ecc7b5415378afe9dc8dc8e
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-22 15:45:38 +0900

    Create lv2_최솟값 만들기.java

---
+++ Programmers/JAVA/lv2/lv2_최솟값 만들기.java
@@ -0,0 +1,19 @@
+https://programmers.co.kr/learn/courses/30/lessons/12941?language=java
+
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i = 0; i < A.length; i++){
+            answer += A[i] * B[A.length-1-i];
+        }
+
+        return answer;
+    }
+}
