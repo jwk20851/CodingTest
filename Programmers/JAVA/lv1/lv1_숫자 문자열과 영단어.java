59140e85d59501fdab489db0e992a85abf32374f
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-28 16:44:22 +0900

    Create lv1_숫자 문자열과 영단어.java

---
+++ Programmers/JAVA/lv1/lv1_숫자 문자열과 영단어.java
@@ -0,0 +1,15 @@
+https://programmers.co.kr/learn/courses/30/lessons/81301
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < num.length; i++){
+            s = s.replace(num[i], Integer.toString(i));
+        }
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
