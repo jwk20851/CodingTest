9b6a40c771f1cc279adcd923024947d78376d348
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-06-29 14:17:24 +0900

    Create lv1_상위 n개 레코드.sql

---
+++ Programmers/SQL/lv1_상위 n개 레코드.sql
@@ -0,0 +1,10 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59405
+
+-- Oracle
+SELECT NAME FROM (SELECT * FROM ANIMAL_INS ORDER BY DATETIME) WHERE ROWNUM = 1;
+
+-- MySQL
+SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
