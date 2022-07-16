9e4091c2ef148d54b83ac241028d835513b1eaad
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-16 16:20:55 +0900

    Create B5_고양이.cs

---
+++ Baekjoon/C#/B5_고양이.cs
@@ -0,0 +1,14 @@
+https://www.acmicpc.net/problem/10171
+
+using System;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
