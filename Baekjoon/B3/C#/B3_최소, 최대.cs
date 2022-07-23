6399baec536332f7a968f070eea49a4cf9506867
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-23 14:52:42 +0900

    Create B3_최소, 최대.cs

---
+++ Baekjoon/B3/C#/B3_최소, 최대.cs
@@ -0,0 +1,24 @@
+https://www.acmicpc.net/submit/10818
+
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            string[] str = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(str[i]);
+            }
+            Array.Sort(arr);
+
+            Console.WriteLine($"{arr[0]} {arr[n - 1]}");
+        }
+    }
+}
