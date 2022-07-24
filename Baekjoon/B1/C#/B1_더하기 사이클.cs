507601ec212a3187156be79bcef68cb4b766d76b
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-24 21:02:17 +0900

    Create B1_더하기 사이클.cs

---
+++ Baekjoon/B1/C#/B1_더하기 사이클.cs
@@ -0,0 +1,21 @@
+https://www.acmicpc.net/submit/1110
+
+using System;
+
+namespace Baekjoon{
+    class Program{
+        static void Main(string[] args){
+            int a = int.Parse(Console.ReadLine());
+            int b = a;
+            int sum = 0, cnt = 0;
+            
+            do{
+                sum = (b/10) + (b%10);
+                b = (b%10)*10 + sum%10;
+                cnt++;
+            }while(a != b);
+            
+            Console.WriteLine(cnt);
+        }
+    }
+}
