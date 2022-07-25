0fb00c7038c783c51246b4584b355af3933737fd
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-25 14:53:22 +0900

    Create B2_OX퀴즈.java

---
+++ Baekjoon/B2/JAVA/B2_OX퀴즈.java
@@ -0,0 +1,27 @@
+https://www.acmicpc.net/submit/8958
+
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        
+		String str = "";
+        int cnt = 0, sum = 0;
+		for(int i=0; i<n; i++) {
+            cnt = 0;
+			sum = 0;
+			str = sc.next();
+			for(int j=0; j<str.length(); j++) {
+				if(str.charAt(j) == 'O') {
+					cnt++;
+					sum += cnt;
+                }
+				else
+					cnt = 0;
+			}
+			System.out.println(sum);
+		}
+	}
+}
