79c3df17aab683600b71ca06090258685bba5d37
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-27 14:36:11 +0900

    Create B1_평균은 넘겠지.cs

---
+++ Baekjoon/B1/C#/B1_평균은 넘겠지.cs
@@ -0,0 +1,39 @@
+https://www.acmicpc.net/submit/4344
+
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            float sum = 0;
+            float cnt = 0;
+            float aver = 0;
+            float answer = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+                sum = 0;
+                cnt = 0;
+                for (int j = 1; j < str.Length; j++)
+                {
+                    sum += int.Parse(str[j]);
+                }
+                aver = sum / float.Parse(str[0]);
+
+                for (int k = 1; k < str.Length; k++)
+                {
+                    if (int.Parse(str[k]) > aver)
+                        cnt++;
+                }
+                answer = cnt / float.Parse(str[0]);
+
+                Console.WriteLine("{0:#0.000%}", answer);
+            }
+        }
+    }
+}
