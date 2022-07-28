1b76a9b5d857075bf997714bc73447d52d88ca3f
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-28 17:06:48 +0900

    Create B2_숫자의 개수.cs

---
+++ Baekjoon/B2/C#/B2_숫자의 개수.cs
@@ -0,0 +1,29 @@
+https://www.acmicpc.net/submit/2577
+
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int n = a * b * c;
+            char[] numArr = n.ToString().ToCharArray();
+            int[] arr = new int[10];
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                arr[int.Parse(numArr[i].ToString())]++;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
