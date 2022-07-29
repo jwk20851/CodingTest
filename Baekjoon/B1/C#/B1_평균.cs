ea785513d46030504cb8766992d0415bc5d3e6b9
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-29 16:48:26 +0900

    Create B1_평균.cs

---
+++ Baekjoon/B1/C#/B1_평균.cs
@@ -0,0 +1,31 @@
+https://www.acmicpc.net/submit/1546
+
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arr = Console.ReadLine().Split(' ');
+            float[] score = Array.ConvertAll(arr, float.Parse);
+            float max = 0;
+            for (int i = 0; i < score.Length; i++)
+            {
+                if (max < score[i])
+                    max = score[i];
+            }
+
+            float sum = 0;
+            for (int i = 0; i < score.Length; i++)
+            {
+                score[i] = score[i] / max * 100;
+                sum += score[i];
+            }
+
+            Console.WriteLine(sum / score.Length);
+        }
+    }
+}
