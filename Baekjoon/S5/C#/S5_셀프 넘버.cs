cca0a92d7c6098d5088da20a77fe845da4e719c6
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-31 18:01:21 +0900

    Create S5_셀프 넘버.cs

---
+++ Baekjoon/S5/C#/S5_셀프 넘버.cs
@@ -0,0 +1,35 @@
+https://www.acmicpc.net/submit/4673
+
+using System;
+using System.Text;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool[] arr = new bool[10001];
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (cr(i) <= 10000)
+                    arr[cr(i)] = true;
+                if (arr[i] == false)
+                    sb.Append(i + "\n");
+            }
+            Console.WriteLine(sb);
+        }
+
+        static int cr(int a)
+        {
+            int m = a;
+            while (a > 0)
+            {
+                m = m + a % 10;
+                a /= 10;
+            }
+            return m;
+        }
+    }
+}
