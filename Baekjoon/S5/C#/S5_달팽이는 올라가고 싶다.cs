b522c451c9f0ead3c39500214b6bd38afbeec071
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-07-31 18:01:24 +0900

    Create S5_달팽이는 올라가고 싶다.cs

---
+++ Baekjoon/S5/C#/S5_달팽이는 올라가고 싶다.cs
@@ -0,0 +1,16 @@
+https://www.acmicpc.net/submit/2869
+
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int a = arr[0], b = arr[1], v = arr[2];
+            Console.WriteLine(((v - b) % (a - b) == 0) ? (v - b) / (a - b) : (v - b) / (a - b) + 1);
+        }
+    }
+}
