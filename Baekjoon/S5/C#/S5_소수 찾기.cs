f5b6968c003164361131ad91d3b1573088476337
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-08-01 17:02:33 +0900

    Create S5_소수 찾기.cs

---
+++ Baekjoon/S5/C#/S5_소수 찾기.cs
@@ -0,0 +1,34 @@
+https://www.acmicpc.net/submit/1978
+
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arrStr = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arrStr, int.Parse);
+
+            int ctr = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] < 2) continue;
+                for (int j = 2; j <= Math.Sqrt(arr[i]); j++)
+                {
+                    if (arr[i] % j == 0)
+                    {
+                        arr[i] = 0;
+                        break;
+                    }
+                }
+                if (arr[i] != 0)
+                    ctr++;
+            }
+
+            Console.WriteLine(ctr);
+        }
+    }
+}
