b7908f50c4184ce7a6c72aa154b2934bce9be436
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-09-26 23:17:33 +0900

    Create S5_마트료시카 합치기.cs

---
+++ Baekjoon/S5/C#/S5_마트료시카 합치기.cs
@@ -0,0 +1,42 @@
+https://www.acmicpc.net/problem/25631
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Beakjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            List<int> li = arr.ToList();
+            li.Sort();
+
+            if (li[0] == li[li.Count - 1])
+            {
+                Console.Write(li.Count);
+                return;
+            }
+
+            int cnt = 1;
+            int max = 0;
+            while (li.Count > 0)
+            {
+                li.RemoveAt(0);
+
+                if (li.Count > 1 && li[0] == li[1])
+                    cnt++;
+                else
+                    cnt = 1;
+
+                max = Math.Max(max, cnt);
+            }
+
+            Console.Write(max);
+        }
+    }
+}
