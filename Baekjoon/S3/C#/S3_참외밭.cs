ba54d1a3910c9afb8c567ee5b24c5f71ddd8486b
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-11-05 00:12:28 +0900

    Update S3_참외밭.cs

--- Baekjoon/S3/C#/S3_참외밭.cs
+++ Baekjoon/S3/C#/S3_참외밭.cs
@@ -9,36 +9,27 @@ namespace Baekjoon
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int maxX = 0, maxY = 0;
-            int[] length = new int[6];
+            int maxX = 0, maxY = 0, idxX = 0, idxY = 0;
+            int[] dis = new int[6];
 
             for (int i = 0; i < 6; i++)
             {
                 int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                dis[i] = arr[1];
                 if (arr[0] == 1 || arr[0] == 2)
+                {
                     maxX = Math.Max(maxX, arr[1]);
-                else
-                    maxY = Math.Max(maxY, arr[1]);
-                length[i] = arr[1];
-            }
-            int big = maxX * maxY;
-
-            int small = 1;
-            int cnt = 0;
-            for (int i = 0; i < 6; i++)
-            {
-                if (length[i] == maxX || length[i] == maxY)
-                    continue;
+                    idxX = i;
+                }
                 else
                 {
-                    if (cnt == 0) cnt++;
-                    else if (cnt < 3)
-                    {
-                        small *= length[i];
-                        cnt++;
-                    }
+                    maxY = Math.Max(maxY, arr[1]);
+                    idxY = i;
                 }
             }
+
+            int big = maxX * maxY;
+            int small = dis[(idxX + 3) % 6] * dis[(idxY + 3) % 6];
 
             Console.Write((big - small) * n);
         }
