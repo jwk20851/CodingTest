ad52a660d684dbfb0456c07b9bd9bd804ed2c429
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-12-13 23:59:13 +0900

    Create S5_거스름돈.cs

---
+++ Baekjoon/S5/C#/S5_거스름돈.cs
@@ -0,0 +1,26 @@
+https://www.acmicpc.net/problem/14916
+
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] dp = new int[n + 1];
+
+            Array.Fill(dp, Int32.MaxValue);
+
+            dp[2] = 1;
+            dp[4] = 2;
+            dp[5] = 1;
+
+            for (int i = 6; i <= n; i++)
+                dp[i] = Math.Min(dp[i - 2], dp[i - 5]) + 1;
+
+            Console.WriteLine((dp[n] == Int32.MaxValue) ? -1 : dp[n]);
+        }
+    }
+}
