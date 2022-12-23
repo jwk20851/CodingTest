1757e68317d29e706e70be2aaf6e5ed8fed43203
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2022-12-24 01:10:36 +0900

    Create S4_전투의 신.cs

---
+++ Baekjoon/S4/C#/S4_전투의 신.cs
@@ -0,0 +1,35 @@
+https://www.acmicpc.net/problem/26595
+
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int a = arr[0], pa = arr[1], b = arr[2], pb = arr[3];
+
+            int cntA = 0, cntB = 0, sum = 0, max = 0;
+            int ansA = 0, ansB = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                cntA = i / pa;
+                cntB = (n - i) / pb;
+
+                sum = a * cntA + b * cntB;
+                if (sum >= max)
+                {
+                    max = sum;
+                    ansA = cntA;
+                    ansB = cntB;
+                }
+            }
+
+            Console.WriteLine(ansA + " " + ansB);
+        }
+    }
+}
