bdfaf4b8ffed6d94dad8b8417a1b1127f1ad9547
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-02-05 17:07:21 +0900

    Update S4_올바른 배열.java

--- Baekjoon/S4/JAVA/S4_올바른 배열.java
+++ Baekjoon/S4/JAVA/S4_올바른 배열.java
@@ -3,7 +3,6 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
