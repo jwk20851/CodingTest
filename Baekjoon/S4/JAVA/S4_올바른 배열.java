cff6a6f99e39a142860126921e857036299ce2bd
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-02-05 17:00:35 +0900

    Update S4_올바른 배열.java

--- Baekjoon/S4/JAVA/S4_올바른 배열.java
+++ Baekjoon/S4/JAVA/S4_올바른 배열.java
@@ -1,37 +1,36 @@
 //https://www.acmicpc.net/problem/1337
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Main
-{
-	public static void main(String[] args) throws IOException {
+public class Main{
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        ArrayList<Integer> li = new ArrayList<>();
-        for(int i = 0; i < n; i++)
-          li.add(Integer.parseInt(br.readLine()));
-        
-        Collections.sort(li);
-
-        int[] cnt = new int[n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < 5; j++) {
-                if (!li.contains(li.get(i) + j))
-                    cnt[i]++;
+		int n = Integer.parseInt(br.readLine());
+		
+		List<Integer> li = new ArrayList<>();
+		for(int i = 0; i < n; i++)
+		    li.add(Integer.parseInt(br.readLine()));
+		
+		Collections.sort(li);
+		
+		int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j <= 4; j++){
+                if(!li.contains(li.get(i)+j))
+                    arr[i]++;
             }
         }
-
-        int min = cnt[0];
-        for (int c : cnt) {
-            if (c <= min)
-                min = c;
-        }
-        
-        System.out.println(min);
+		
+		int min = arr[0];
+		for(int v : arr)
+		    min = Math.min(v, min);
+		
+		System.out.print(min);
 	}
 }
