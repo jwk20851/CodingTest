321be84ef16442168b1cdbb66bcce5ed4b3f84a3
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-02-09 23:48:35 +0900

    Create S3_N과 M (7).java

---
+++ Baekjoon/S3/JAVA/S3_N과 M (7).java
@@ -0,0 +1,47 @@
+//https://www.acmicpc.net/problem/15656
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main{
+    static int n = 0, m = 0;
+    static int[] arr, ans;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n];
+		ans = new int[m];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++)
+		    arr[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(arr);
+		
+		DFS(0);
+		
+		System.out.print(sb);
+	}
+	
+	static void DFS(int dep){
+	    if(dep == m){
+	        for(int i = 0; i < m; i++)
+	            sb.append(ans[i] +" ");
+	        sb.append("\n");
+	        return;
+	    }
+	    
+        for(int i = 0; i < n; i++){
+            ans[dep] = arr[i];
+            DFS(dep+1);
+	    }
+	}
+}
