83bbb930b5aa2939089be7fcbf70866b8695b65c
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-02-23 17:33:24 +0900

    Update S1_극장 좌석.java

--- Baekjoon/S1/JAVA/S1_극장 좌석.java
+++ Baekjoon/S1/JAVA/S1_극장 좌석.java
@@ -10,8 +10,8 @@ public class Main{
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
         
-        int[] dp = new int[n+1];
-        dp[1] = 1;
+        int[] dp = new int[41];
+        dp[0] = dp[1] = 1;
         dp[2] = 2;
         for(int i = 3; i <= n; i++)
             dp[i] = dp[i-1] + dp[i-2];
