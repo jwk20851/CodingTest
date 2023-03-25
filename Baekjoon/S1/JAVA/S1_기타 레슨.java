0faa18629e2da660506132115bc6b4eff1bacdf8
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-03-25 22:55:48 +0900

    Update S1_기타 레슨.java

--- Baekjoon/S1/JAVA/S1_기타 레슨.java
+++ Baekjoon/S1/JAVA/S1_기타 레슨.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
-    private static char[] arr;
 	public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
