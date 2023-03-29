bd381a9ac228775c0344f2e976ce621a85f9a389
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-03-29 22:24:52 +0900

    Create S3_이상한 술집.java

---
+++ Baekjoon/S3/JAVA/S3_이상한 술집.java
@@ -0,0 +1,38 @@
+//https://www.acmicpc.net/problem/13702
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] mak = new int[N];
+        for(int i = 0; i < N; i++)
+            mak[i] = Integer.parseInt(br.readLine());
+        
+        long l = 1, r = mak[N-1], ans = 0;
+        while(l <= r){
+            long mid = (l+r)/2;
+                
+            long cnt = 0;
+            for(int i = 0; i < N; i++)
+                cnt += mak[i]/mid;
+            
+            if(cnt < K)
+                r = mid -1;
+            else{
+                ans = mid;
+                l = mid +1;
+            }
+        }
+        
+        System.out.print(ans);
+    }
+}
