26c48549476f6f02b92fe4ea05f3a1481e57945b
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-04-10 00:04:04 +0900

    Create S5_행렬 곱셈.java

---
+++ Baekjoon/S5/JAVA/S5_행렬 곱셈.java
@@ -0,0 +1,45 @@
+//https://www.acmicpc.net/problem/2740
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] arrA = new int[N][M];
+		for(int i = 0; i < N; i++){
+		    st = new StringTokenizer(br.readLine());
+		    for(int j = 0; j < M; j++)
+		        arrA[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[][] arrB = new int[M][K];
+		for(int i = 0; i < M; i++){
+		    st = new StringTokenizer(br.readLine());
+		    for(int j = 0; j < K; j++)
+		        arrB[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++){
+		    for(int j = 0; j < K; j++){
+		        int sum = 0;
+		        for(int k = 0; k < M; k++){
+		            sum += arrA[i][k]*arrB[k][j];
+		        }
+		        sb.append(sum+ " ");
+		    }
+		    sb.append("\n");
+		}
+		
+        System.out.print(sb);
+	}
+}
