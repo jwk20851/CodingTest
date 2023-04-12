80abeb7a4835ff4186b7e5ec3d7937a7eef90bbc
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-04-12 23:59:33 +0900

    Create S3_이전 수열.java

---
+++ Baekjoon/S3/JAVA/S3_이전 수열.java
@@ -0,0 +1,60 @@
+//https://www.acmicpc.net/problem/10973
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main{
+    private static int[] arr;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++)
+		    arr[i] = Integer.parseInt(st.nextToken());
+		
+		if(search()){
+			for(int i = 0; i < N; i++)
+				System.out.print(arr[i] + " ");
+		}
+		else
+			System.out.print(-1);
+	}
+	
+	public static boolean search(){
+	    int a = arr.length -1;
+		while(a>0 && arr[a]>=arr[a-1])
+			a--;
+
+		if(a <= 0)
+			return false;
+
+		int b = arr.length -1;
+		while(arr[a-1] <= arr[b])
+			b--;
+		
+		swap(a-1, b);
+		int tmp = arr[a-1];
+		arr[a-1] = arr[b];
+		arr[b] = tmp;
+
+		b = arr.length -1;
+		while(a < b){
+		    swap(a, b);
+			a++;
+			b--;
+		}
+		
+		return true;
+	}
+	
+	public static void swap(int a, int b){
+	    int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
+}
