8538c9972d6f8f81237141c8286c170c9e6808db
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-04-26 17:31:51 +0900

    Update lv1_크레인 인형뽑기 게임.java

--- Programmers/JAVA/lv1/lv1_크레인 인형뽑기 게임.java
+++ Programmers/JAVA/lv1/lv1_크레인 인형뽑기 게임.java
@@ -1,25 +1,27 @@
-https://programmers.co.kr/learn/courses/30/lessons/64061
+//https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=java
 
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i = 0; i < moves.length; i++){
+        for(int i : moves){
             for(int j = 0; j < board.length; j++){
-                if(board[j][moves[i]-1] > 0){
-                    if (!(stack.isEmpty()) && stack.peek() == board[j][moves[i]-1]){
-                        stack.pop();
-                        answer += 2;
-                    }
-                    else{
-                        stack.push(board[j][moves[i]-1]);
-                    }
-                    board[j][moves[i]-1] = 0;
-                    break;
+                if(board[j][i-1] == 0)
+                    continue;
+                
+                if(st.peek() == board[j][i-1]){
+                    st.pop();
+                    answer += 2;
                 }
+                else{
+                    st.push(board[j][i - 1]);
+                }
+                
+                board[j][i-1] = 0;
+                break;
             }
         }
         
