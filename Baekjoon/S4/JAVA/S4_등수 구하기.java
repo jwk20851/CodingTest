7c14daa6e2b42569ea6bdb560a589be7c7d7e4dc
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-05-01 23:59:29 +0900

    Create S4_등수 구하기.java

---
+++ Baekjoon/S4/JAVA/S4_등수 구하기.java
@@ -0,0 +1,36 @@
+//https://www.acmicpc.net/problem/1205
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int score = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++)
+		    arr[i] = Integer.parseInt(st.nextToken());
+		    
+		if(N==P && arr[N-1]>=score){
+			System.out.println(-1);
+			System.exit(0);
+		}
+		
+		int rank = 1;
+		for(int i = 0; i < N; i++){ 
+			if(arr[i] > score)
+				rank++;
+			else
+				break;
+		}
+	
+		System.out.println(rank);
+	}
+}
