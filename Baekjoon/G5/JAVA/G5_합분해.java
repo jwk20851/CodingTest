45ee90651dc13caa65a0e90a104aeb929a1e2a7e
jwk20851 <93560419+jwk20851@users.noreply.github.com>
2023-05-14 22:09:36 +0900

    Create G5_합분해.java

---
+++ Baekjoon/G5/JAVA/G5_합분해.java
@@ -0,0 +1,29 @@
+//https://www.acmicpc.net/problem/2225
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[][] dp = new int[N+1][K+1];
+        for(int i = 1; i <= N; i++)
+            dp[i][1] = 1;
+		Arrays.fill(dp[0], 1);
+        
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= K; j++){
+                dp[i][j] = (dp[i-1][j]+dp[i][j-1])%1000000000;
+            }
+        }
+        
+		System.out.print(dp[N][K]);
+	}
+}
