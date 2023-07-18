c6314a1403583b66fbd63fd2dcf03c75fe794d9f
정원규 <jwk20851@gmail.com>
2023-07-18 23:21:51 +0900

    Create S5_수 정렬하기 5.java

---
+++ Baekjoon/S5/JAVA/S5_수 정렬하기 5.java
@@ -0,0 +1,25 @@
+//https://www.acmicpc.net/problem/15688
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++)
+		    arr[i] = Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int n : arr)
+	        sb.append(n).append("\n");	//sb.append(n+"\n") 경우 시간초과
+		
+		System.out.print(sb);
+    }
+}
