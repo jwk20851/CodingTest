b6d82ac3ab215cbe1a579a6819b24d69ea7dbc8c
정원규 <jwk20851@gmail.com>
2023-08-08 23:27:35 +0900

    Sql files Updte

--- Programmers/SQL/lv1_동물의 아이디와 이름.sql
+++ Programmers/SQL/lv1_동물의 아이디와 이름.sql
@@ -1,7 +1,11 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59403?language=mysql
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59403
 
 -- Oracle
-SELECT ANIMAL_ID, NAME FROM ANIMAL_INS ORDER BY ANIMAL_ID;
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
 
 -- MySQL
-SELECT ANIMAL_ID, NAME FROM ANIMAL_INS ORDER BY ANIMAL_ID;
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
---
+++ Programmers/SQL/lv2_동물 수 구하기.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59406
+
+-- Oracle
+SELECT COUNT(*)
+FROM ANIMAL_INS
+
+-- MySQL
+SELECT COUNT (*)
+FROM ANIMAL_INS
