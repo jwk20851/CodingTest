20f41f86028bb27ceb3495c3e9ce440742cf068f
정원규 <jwk20851@gmail.com>
2023-09-17 23:58:44 +0900

    Create S2_팩토리얼5.java

---
+++ Baekjoon/S2/JAVA/S2_팩토리얼5.java
@@ -0,0 +1,25 @@
+//https://www.acmicpc.net/problem/1564
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		long num = 1;
+        long modV = (long)1e12;
+        for(int i = 1; i <= N; i++){
+            num *= i;
+            while(num%10 == 0)
+                num/=10;
+                
+            num %= modV;
+        }
+        
+        String tmp = Long.toString(num);
+        System.out.println(tmp.substring(tmp.length()-5, tmp.length()));
+    }
+}
