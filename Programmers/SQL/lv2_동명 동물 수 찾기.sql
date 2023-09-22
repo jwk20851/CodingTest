2e6dec3f45b33eb6b59d1425261bf34eed8f338e
정원규 <jwk20851@gmail.com>
2023-09-22 23:54:51 +0900

    Create lv2_동명 동물 수 찾기.sql

---
+++ Programmers/SQL/lv2_동명 동물 수 찾기.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59041
+
+-- Oracle, MySQL 
+SELECT NAME, COUNT(NAME) AS COUNT
+FROM ANIMAL_INS
+GROUP BY NAME
+HAVING COUNT(NAME) >= 2
+ORDER BY NAME
