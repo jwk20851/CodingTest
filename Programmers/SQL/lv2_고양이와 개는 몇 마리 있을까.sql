8c74521be36477be7945f8d87141f7a375129164
정원규 <jwk20851@gmail.com>
2023-09-30 01:28:22 +0900

    Sql Update

---
+++ Programmers/SQL/lv2_고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59040
+
+-- Oracle, MySQL
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) AS count
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE;
